ecc16908e89e7f080b594d0e619ae9fcb4f0175a
Morgan Pritchard <morgan.pritchard@nwktc.edu>
2024-10-05 12:10:29 -0600

    Init

---
+++ admin/admin.js
@@ -0,0 +1,44 @@
+var approve = document.getElementById('approveCard_1');
+var deny = document.getElementById('denyCard_1');
+var cardHolder = document.getElementById('cardholder');
+var section = document.getElementById('containerSection');
+var sidenavtrigger = document.getElementById('sidenav-trigger');
+approve.addEventListener('click', function() {
+    alert('Approved');
+    // TODO: Remove the appropriate card.
+    cardHolder.innerHTML = '<h4>Pending Posts</h4>';
+})
+deny.addEventListener('click', function() {
+    alert('Denied');
+    // TODO: Remove the appropriate card.
+    cardHolder.innerHTML = '<h4>Pending Posts</h4>';
+})
+
+
+changeScreenSize(window.matchMedia("(max-width: 900px)").matches);
+window.matchMedia("(max-width: 900px)").onchange = function (e) {
+    changeScreenSize(e.matches);
+}
+
+
+function changeScreenSize(isMobile) {
+    if(isMobile) {
+        section.classList.remove('row')
+        section.classList.add('col')
+    } else {
+        section.classList.remove('col')
+        section.classList.add('row')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav, {
+        edge: 'left',
+        draggable: true
+    });
+    // instances[0].open();
+    sidenavtrigger.addEventListener('click', function() {
+        instances[0].open();
+    })
+});
---
+++ main.js
@@ -0,0 +1,41 @@
+var loginText = document.getElementById('loginText');
+var loginSection = document.getElementById('loginSection');
+
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+var loginButton = document.getElementById('loginButton');
+
+
+
+changeScreenSize(window.matchMedia("(max-width: 900px)").matches);
+window.matchMedia("(max-width: 900px)").onchange = function (e) {
+    changeScreenSize(e.matches);
+}
+
+loginButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    if(username.value == 'admin' && password.value == 'admin') {
+        window.location.href = './admin/admin.html';
+    } else if(username.value == 'student' && password.value == 'student') {
+        window.location.href = './student/student.html';
+    }
+    else {
+        alert('Invalid Login');
+    }
+});
+
+
+
+
+
+function changeScreenSize(isMobile) {
+    if(isMobile) {
+        loginText.classList.remove('col', 's6')
+        loginSection.classList.remove('s6')
+        loginSection.classList.add('s12')
+    } else {
+        loginText.classList.add('col', 's6')
+        loginSection.classList.add('s6')
+        loginSection.classList.remove('s12')
+    }
+}
