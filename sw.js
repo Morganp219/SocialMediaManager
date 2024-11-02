7fc5e2f7721f7a0a9fdcf6c7e7875e10733bf025
Morgan Pritchard <morgan.pritchard@nwktc.edu>
2024-11-01 20:29:55 -0600

    Finished Service Worker

--- sw.js
+++ sw.js
@@ -4,15 +4,14 @@ self.addEventListener("install", event => {
     caches.open("socialmediamgr").then(async cache => {
                
         // Home Page
-        await cache.addAll(["./index.html", "./styles/style.css", "./main.js"]);
-        // Student Side
-        await cache.addAll(["./student/student.html", "./student/student.css", "./student/student.js"])
-        //Admin Side
-        await cache.addAll(["./admin/admin.html", "./admin/admin.css", "./admin/admin.js", "./admin/users.html"])
-        // Images & Assets
-        await cache.addAll(["./manifest.json", "./images/icon.png", "./images/Designer.jpeg", "./images/pexels-barbland-28260049.jpg", "./images/pexels-personone.jpg", "./images/pexels-pixabay-270404.jpg", "./images/pexels-pixabay-302769.jpg"])
-        console.log("Caching complete");
-        
+        try {
+            await cache.addAll(["./index.html", "./styles/style.css", "./main.js", "./styles/materialize.min.css", "./styles/materialize.min.js", "./student/student.html", "./student/student.css", "./student/student.js", "./admin/admin.html", "./admin/admin.css", "./admin/admin.js", "./admin/users.html", "./manifest.json", "./images/icon.png", "./images/Designer.jpeg", "./images/pexels-barbland-28260049.jpg", "./images/pexels-personone.jpg", "./images/pexels-pixabay-270404.jpg", "./images/pexels-pixabay-302769.jpg"])
+            console.log("Caching complete");
+
+        }    catch (err) {
+            console.error(err);
+        }
+
     })
 
 }
@@ -20,4 +19,25 @@ self.addEventListener("install", event => {
 });
  self.addEventListener("activate", event => {
     console.log("Service worker activated");
+    event.waitUntil(
+        (async () => {
+            const cacheNames = await caches.keys();
+            await Promise.all(cacheNames
+                .filter(cacheName => cacheName !== "socialmediamgr")
+                .map(cacheName => caches.delete(cacheName)));
+                        
+    })
+    );
  });
+
+ self.addEventListener("fetch", event => {
+    event.respondWith(
+        (async () => {
+            const cachedResponse = await caches.match(event.request);
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })()
+    );
+ })
